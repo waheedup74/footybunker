d1e9f602768ac19de1b7a4687dffc894cb7e702e
waheedup74 <waheedup74@gmail.com>
2023-03-09 05:54:03 +0000

    error fixed, today match missing players stats error

--- components/TeamStats.vue
+++ components/TeamStats.vue
@@ -27,7 +27,7 @@ const months6Before = ref("");
 let today = new Date();
 
 const getDate = function (date) {
-  let dd = String(date.getDate()).padStart(2, "0");
+  let dd = String(date.getDate() - 1).padStart(2, "0");
   let mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
   let yyyy = date.getFullYear();
 
