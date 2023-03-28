5dddbb4c3139279724680ab95f48fe558cdddf66
waheedup74 <waheedup74@gmail.com>
2023-03-28 12:19:11 +0500

    standing error removed

--- components/TeamStandings.vue
+++ components/TeamStandings.vue
@@ -11,7 +11,7 @@ const goto = function (team) {
 </script>
 
 <template>
-  <div class="w-9/10 md:w-4/5 mx-auto">
+  <div class="w-9/10 md:w-4/5 mx-auto py-10">
     <!-- add section -->
     <div
       class="bg-rose-500 text-white rounded-lg py-2 px-2 my-5 transition delay-150 duration-300 ease-in-out"
@@ -21,40 +21,6 @@ const goto = function (team) {
       </div>
     </div>
     <!-- add section -->
-
-    <!-- <div v-if="teams">
-      <div class="grid grid-cols-8 gap-2 shadow-0">
-        <div class="p-1">Logo</div>
-        <div class="p-1">Name</div>
-        <div class="p-1">Played</div>
-        <div class="p-1">Won</div>
-        <div class="p-1">Draw</div>
-        <div class="p-1">Lost</div>
-        <div class="p-1">Goal Scored</div>
-        <div class="p-1">Points</div>
-      </div>
-      <div
-        class="grid grid-cols-8 p-2 gap-2 border mb-2 hover:bg-slate-300 hover:cursor-pointer"
-        v-for="team in teams.data[0].standings.data"
-        :key="team.team_id"
-        @click="goto(team.team_id)"
-      >
-        <div class="p-1 self-center">
-          <img :src="team.team.data.logo_path" class="w-10" alt="" />
-        </div>
-
-        <div class="p-1 self-center">{{ team.team_name }}</div>
-        <div class="p-1 self-center">{{ team.overall.games_played }}</div>
-        <div class="p-1 self-center">{{ team.overall.won }}</div>
-        <div class="p-1 self-center">{{ team.overall.draw }}</div>
-        <div class="p-1 self-center">{{ team.overall.lost }}</div>
-        <div class="p-1 self-center">{{ team.overall.goals_scored }}</div>
-        <div class="p-1 self-center">{{ team.total.points }}</div>
-      </div>
-    </div>
-    <div v-else>
-      {{ testError }}
-    </div> -->
 
     <div v-if="teams">
       <table class="w-full">
@@ -78,22 +44,28 @@ const goto = function (team) {
           <th class="text-start">G</th>
           <th class="text-start">Pts</th>
         </thead>
-        <tbody
-          v-for="team in teams.data[0].standings.data"
-          :key="team.team_id"
-          @click="goto(team.team_id)"
-          class="border cursor-pointer"
-        >
-          <td>
-            <img :src="team.team.data.logo_path" class="h-6 w-6 mx-auto" alt="logo" />
-          </td>
-          <td>{{ team.team_name }}</td>
-          <td>{{ team.overall.games_played }}</td>
-          <td>{{ team.overall.won }}</td>
-          <td>{{ team.overall.draw }}</td>
-          <td>{{ team.overall.lost }}</td>
-          <td>{{ team.overall.goals_scored }}</td>
-          <td>{{ team.total.points }}</td>
+        <tbody>
+          <tr
+            v-for="team in teams.data[0]?.standings.data"
+            :key="team.team_id"
+            @click="goto(team.team_id)"
+            class="border cursor-pointer py-4"
+          >
+            <td>
+              <img
+                :src="team.team.data.logo_path"
+                class="h-6 w-6 mx-auto"
+                alt="logo"
+              />
+            </td>
+            <td>{{ team.team_name }}</td>
+            <td>{{ team.overall.games_played }}</td>
+            <td>{{ team.overall.won }}</td>
+            <td>{{ team.overall.draw }}</td>
+            <td>{{ team.overall.lost }}</td>
+            <td>{{ team.overall.goals_scored }}</td>
+            <td>{{ team.total.points }}</td>
+          </tr>
         </tbody>
       </table>
     </div>
@@ -102,3 +74,9 @@ const goto = function (team) {
     </div>
   </div>
 </template>
+
+<style scoped>
+tbody td {
+  padding: 10px 0;
+}
+</style>
