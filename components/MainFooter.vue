cd054d008156dd7dc2eb881923ad004c07e0ef0b
waheedup74 <waheedup74@gmail.com>
2023-05-10 10:46:03 +0100

    footer image added

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="mt-auto fixed bottom-0 w-full z-50">
-    <div class="flex justify-between bg-black">
-      <div class="py-3 mx-auto z-auto">
-        <img src="@/assets/fb-logo.png" class="h-10" alt="app-logo" />
+  <div class="mt-auto fixed bottom-0 w-full z-50 px-8 bg-black">
+    <div class="flex justify-between">
+      <div class="py-3 z-auto">
+        <img src="@/assets/footer.png" class="h-10" alt="app-logo" />
       </div>
-      <div>
-        <p>This is text</p>
+      <div class="self-center text-white">
+        <div>
+          © 2023 <a href="http://betbuilders.net/">BetBuilders.net</a> | 18+
+          <a href="https://www.begambleaware.org/">begambleaware.org</a>
+        </div>
       </div>
     </div>
   </div>
